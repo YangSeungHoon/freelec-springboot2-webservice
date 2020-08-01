6b42b0752aaf4d5048106d0333849f76b39262da
YangSeungHoon <sheepsh93@gmail.com>
2020-08-01 18:29:04 +0900

    After Refactoring // Spring-session-jdbc 등록 // Naver-login

--- src/main/java/com/sh/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sh/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -10,32 +10,36 @@ import java.util.Map;
 @Getter
 public class OAuthAttributes {
 
-    private Map<String,Object> attributes;
+    private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
     private String email;
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,String nameAttributeKey,String name,
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name,
                            String email, String picture) {
 
-        this.attributes= attributes;
+        this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
-        this.email= email;
-        this.picture= picture;
+        this.email = email;
+        this.picture = picture;
     }
 
 
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
-                                     Map<String,Object> attributes) {
-        return ofGoogle(userNameAttributeName,attributes);
+                                     Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName,
-                                            Map<String,Object> attributes) {
+                                            Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -53,5 +57,18 @@ public class OAuthAttributes {
                 .role(Role.GUEST)
                 .build();
 
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        System.out.println(response+"실행");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 }
